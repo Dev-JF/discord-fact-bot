0995ef04bc79eaf5f4a4eb6c4701d1349e6c0648
Dev-JF <78078546+Dev-JF@users.noreply.github.com>
2022-07-04 12:15:28 +1000

    Update factAPI.py

--- factAPI.py
+++ factAPI.py
@@ -5,7 +5,7 @@ def randomFactCall():
 
     limit = 1
     api_url = 'https://api.api-ninjas.com/v1/facts?limit={}'.format(limit)
-    response = requests.get(api_url, headers={'X-Api-Key': '+vI+MmL3KPXWtMi1afgp/g==KxSX8Ph9oTmbmUIE'})
+    response = requests.get(api_url, headers={'X-Api-Key': 'your api key here'})
     if response.status_code == requests.codes.ok:
         res = response.text
         loadRes = json.loads(res)
