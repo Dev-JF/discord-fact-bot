254990b60775b1f1198d401c66be25cbbb048773
Dev-JF <78078546+Dev-JF@users.noreply.github.com>
2022-07-04 12:16:34 +1000

    Update triviaGeneralQuestion.py

--- triviaGeneralQuestion.py
+++ triviaGeneralQuestion.py
@@ -7,7 +7,7 @@ def triviaGeneral():
     category = 'general'
 
     api_url = 'https://api.api-ninjas.com/v1/trivia?category={}'.format(category)
-    response = requests.get(api_url, headers={'X-Api-Key': '+vI+MmL3KPXWtMi1afgp/g==KxSX8Ph9oTmbmUIE'})
+    response = requests.get(api_url, headers={'X-Api-Key': 'your api key  here'})
     if response.status_code == requests.codes.ok:
        return (response.text)
     else:
